6c461562f192c10ed12235c59c51a1eae95eb165
Siddhant <onlyeeofficial@gmail.com>
2024-12-31 23:28:24 +0530

    Improvements

--- dist/index.js
+++ dist/index.js
@@ -2,11 +2,25 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 8080 });
-wss.on("connection", function (socket) {
-    socket.send("Connected now");
-    socket.on("message", function (e) {
-        if (e.toString() == "ping") {
-            socket.send("pong");
-        }
+let usercount = 0;
+let allSocket = [];
+// Handling server error
+wss.on("error", function error(errors) {
+    console.log(errors);
+});
+//Making connection
+wss.on("connection", function connection(socket) {
+    socket.on("error", console.error);
+    allSocket.push(socket);
+    usercount += 1;
+    socket.send(`Connected users are ${usercount}`);
+    // Relaying messages
+    socket.on("message", function message(e) {
+        setTimeout(() => {
+            for (let i = 0; i < allSocket.length; i++) {
+                const users = allSocket[i];
+                users.send(e.toString());
+            }
+        }, 100);
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,33 @@
-import WebSocket, {WebSocketServer} from "ws";
+import  {WebSocketServer,WebSocket} from "ws";
 
 const wss = new WebSocketServer({port: 8080});
 
-wss.on("connection",function(socket) {
-    socket.send("Connected now");
+let usercount  = 0;
+let allSocket: WebSocket[] = [];
 
-    socket.on("message",function(e){
-        if (e.toString() == "ping") {
-            socket.send("pong")
-        }
+// Handling server error
+wss.on("error",function error(errors){
+    console.log(errors);
+})
+
+//Making connection
+
+wss.on("connection",function connection (socket) {
+    socket.on("error",console.error);
+
+    allSocket.push(socket);
+    usercount +=1;
+    socket.send(`Connected users are ${usercount}`);
+
+
+    // Relaying messages
+    socket.on("message",function message (e){
+        setTimeout(() => {
+            for (let i =0; i<allSocket.length;i++ ){
+                const users = allSocket[i];
+                users.send(e.toString());
+            }
+        }, 100);        
     }
     )
 })
